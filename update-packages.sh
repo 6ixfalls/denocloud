e2713b8bcd7c427b10bd06b865ba1c00a7658752
Six <23470032+6ixfalls@users.noreply.github.com>
2022-06-05 11:40:36 -0700

    Refactor docker image used for package updates

--- update-packages.sh
+++ update-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo rm -r vendor/
-sudo docker container run -v $(pwd):/app -w /app/ lukechannings/deno:latest vendor main.ts relay.ts
+sudo docker container run -v $(pwd):/app -w /app/ sixfalls/deno:latest vendor main.ts relay.ts
