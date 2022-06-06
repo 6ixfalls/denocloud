0c6fae8ad3876bf4eb56542dc8babf4d745acfe8
6ixfalls <bryan888red@gmail.com>
2022-06-05 20:44:15 -0700

    Editor Buttons

---
+++ frontend/src/components/Upload.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+// @ts-ignore
+import UploadStyles from './Upload.module.css'
+
+function Upload({ label, children }: any) {
+    return <h1>WIP</h1>
+}
+
+function Dragger({
+    label,
+    afterLabel,
+    beforeLabel,
+    layout,
+    children,
+    files,
+    setFile,
+}: any) {
+    const [classes, setClasses] = useState([UploadStyles['sbui-upload-dragger']])
+
+    const draggedCssClass = UploadStyles['sbui-upload-dragger--dragged']
+
+    const dragOver = (e: any) => {
+        e.preventDefault()
+
+        if (!classes.includes(draggedCssClass)) {
+            let originalClasses = classes
+            originalClasses.push(draggedCssClass)
+            setClasses(originalClasses)
+        }
+    }
+
+    const dragEnter = (e: any) => {
+        e.preventDefault()
+        if (!classes.includes(draggedCssClass)) {
+            console.log("enter")
+            let originalClasses = classes
+            originalClasses.push(draggedCssClass)
+            setClasses(originalClasses)
+        }
+    }
+
+    const dragLeave = (e: any) => {
+        e.preventDefault()
+
+        if (classes.includes(draggedCssClass)) {
+            let newClasses = classes
+
+            for (var i = 0; i < newClasses.length; i++) {
+                if (newClasses[i] === draggedCssClass) {
+                    newClasses.splice(i, 1)
+                }
+            }
+            console.log("exit")
+            setClasses(newClasses)
+        }
+    }
+
+    const fileDrop = (e: any) => {
+        e.preventDefault()
+        const newFile = e.dataTransfer.files?.[0]
+        setFile(newFile)
+    }
+
+    const fileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        const newFile = e.target.files?.[0]
+        setFile(newFile)
+    }
+
+    return (
+        <div
+            onDragOver={dragOver}
+            onDragEnter={dragEnter}
+            onDragLeave={dragLeave}
+            onDrop={fileDrop}
+        >
+            <label htmlFor="file-upload" className={classes.join(' ')}>
+                <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                    onChange={fileUpload}
+                    accept=".js"
+                />
+
+                {children}
+            </label>
+        </div>
+    )
+}
+
+Upload.Dragger = Dragger
+export default Upload
--- frontend/src/routes/dashboard/project/editor.tsx
+++ frontend/src/routes/dashboard/project/editor.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import CodeMirror from "@uiw/react-codemirror";
 import { basicSetup } from "@codemirror/basic-setup";
 import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
 import { autocompletion, CompletionContext } from "@codemirror/autocomplete";
 import { oneDark } from "./one-dark";
 import { syntaxTree } from "@codemirror/language";
-import { Loading } from "@supabase/ui";
+import { Loading, Button, IconUpload, Modal, Typography } from "@supabase/ui";
+import Upload from "../../../components/Upload";
 
 const completePropertyAfter = ["PropertyName", ".", "?."]
 const dontCompleteIn = ["TemplateString", "LineComment", "BlockComment",
@@ -41,12 +42,15 @@ function completeFromGlobalScope(context: CompletionContext) {
     return null
 }
 
-class EditorComponent extends React.Component<{ project: string }, { code: string | undefined, loading: boolean }> {
+class EditorComponent extends React.Component<{ project: string }, { code: string | undefined, loading: boolean, uploadOpen: boolean, errorOpen: boolean, errorMessage?: string, uploadFile?: File }> {
     constructor(props: any) {
         super(props);
+        this.setFile = this.setFile.bind(this);
         this.state = {
             code: 'console.log("Welcome to DenoCloud!");',
             loading: true,
+            uploadOpen: false,
+            errorOpen: false
         };
     }
 
@@ -59,28 +63,91 @@ class EditorComponent extends React.Component<{ project: string }, { code: strin
         }
     }
 
+    async setFile(file: File) {
+        const fileSizeMB = file.size / 1024 / 1024;
+        if (file.name.endsWith(".js")) {
+            if (fileSizeMB < 10) {
+                this.setState({ uploadFile: file, uploadOpen: false, code: await file.text() });
+            } else {
+                this.setState({ errorOpen: true, errorMessage: "The file you uploaded exceeded the maximum size of 10 MB." });
+            }
+        } else {
+            this.setState({ errorOpen: true, errorMessage: "The file you uploaded isn't a JS file!" });
+        }
+    }
+
     render() {
         return (
-            <Loading active={this.state.loading}>
-                <CodeMirror
-                    value={this.state.code}
-                    className="pt-2"
-                    height="90vh"
-                    theme={oneDark}
-                    extensions={[basicSetup, javascript({ jsx: true, typescript: true }), javascriptLanguage, javascriptLanguage.data.of({ autocompletion: completeFromGlobalScope }), autocompletion()]}
-                    onChange={(value, viewUpdate) => {
-                        console.log("value:", value);
-                    }}
-                />
-            </Loading>
+            <div>
+                <Modal visible={this.state.errorOpen} customFooter={<div className="w-full flex flex-row mt-2"><Button type="outline" onClick={() => this.setState({ errorOpen: false })}>Confirm</Button></div>}>
+                    <span className="w-11/12 mb-2 h-1/2"><h4 className="text-white text-lg font-bold p-5 pb-0">File Error</h4><span className="px-7 pb-2 font-normal text-white">{this.state.errorMessage}</span></span>
+                </Modal>
+                <Modal visible={this.state.uploadOpen} customFooter={<div className="w-full flex flex-row"><Button type="outline" onClick={() => this.setState({ uploadOpen: false })}>Cancel</Button></div>}>
+                    <Upload.Dragger
+                        layout="horizontal"
+                        files={this.state.uploadFile}
+                        setFile={this.setFile}
+                    >
+                        <div className="flex flex-col gap-8 justify-evenly">
+                            <div className="space-y-1 text-center">
+                                <svg
+                                    className="mx-auto h-12 w-12 text-gray-1100"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 48 48"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                                <div className="flex text-sm text-gray-1100">
+                                    <Typography.Text className="m-auto text-center">
+                                        <span className="text-[#3ecf8e] font-normal underline">
+                                            <Typography.Link
+                                                style={{
+                                                    textDecoration: 'none',
+                                                }}
+                                            >
+                                                Upload a file
+                                            </Typography.Link>
+                                        </span>
+
+                                        {' or drag and drop'}
+                                    </Typography.Text>
+                                </div>
+
+                                <p className="text-xs text-gray-900">JS up to 50MB</p>
+                            </div>
+                        </div>
+                    </Upload.Dragger>
+                </Modal>
+                <div className="w-full flex flex-row-reverse mt-3 right-5 absolute top-0">
+                    <Button icon={<IconUpload />} size="medium" className="m-2" onClick={() => this.setState({ uploadOpen: true })}>Upload File</Button>
+                    <Button size="medium" className="m-2"><Link to="editor">Save and Deploy</Link></Button>
+                </div>
+                <Loading active={this.state.loading}>
+                    <CodeMirror
+                        value={this.state.code}
+                        className="pt-2"
+                        height="90vh"
+                        theme={oneDark}
+                        extensions={[basicSetup, javascript({ jsx: true, typescript: true }), javascriptLanguage, javascriptLanguage.data.of({ autocompletion: completeFromGlobalScope }), autocompletion()]}
+                        onChange={(value, viewUpdate) => {
+                            console.log("value:", value);
+                        }}
+                    />
+                </Loading>
+            </div>
         );
     }
 }
 
 export default function Editor() {
     return (
-        <div>
-            <EditorComponent project={useParams().project || ""} />
-        </div>
+        <EditorComponent project={useParams().project || ""} />
     );
 }
