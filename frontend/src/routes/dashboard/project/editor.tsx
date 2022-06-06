c8419427fb53aebc0d6d38bb840f5e5b92115ecf
6ixfalls <bryan888red@gmail.com>
2022-06-05 19:06:05 -0700

    Editor & Project Graphs

--- frontend/src/routes/dashboard/project/editor.tsx
+++ frontend/src/routes/dashboard/project/editor.tsx
@@ -1,30 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
 import CodeMirror from "@uiw/react-codemirror";
+import { basicSetup } from "@codemirror/basic-setup";
 import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
 import { autocompletion, CompletionContext } from "@codemirror/autocomplete";
-import { oneDark } from "@codemirror/theme-one-dark";
-import { syntaxTree } from "@codemirror/language"
+import { oneDark } from "./one-dark";
+import { syntaxTree } from "@codemirror/language";
+import { Loading } from "@supabase/ui";
 
 const completePropertyAfter = ["PropertyName", ".", "?."]
 const dontCompleteIn = ["TemplateString", "LineComment", "BlockComment",
     "VariableDefinition", "PropertyDefinition"]
 
 function completeProperties(from: number, object: Object) {
-    let options = []
-    for (let name in object) {
-        options.push({
-            label: name,
-            //@ts-ignore idk
-            type: typeof object[name] == "function" ? "function" : "variable"
-        })
-    }
     return {
         from,
-        options,
+        options: [],
         validFor: /^[\w$]*$/
     }
 }
 
 function completeFromGlobalScope(context: CompletionContext) {
+    console.log("a");
     let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
 
     if (completePropertyAfter.includes(nodeBefore.name) &&
@@ -44,16 +41,46 @@ function completeFromGlobalScope(context: CompletionContext) {
     return null
 }
 
+class EditorComponent extends React.Component<{ project: string }, { code: string | undefined, loading: boolean }> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            code: 'console.log("Welcome to DenoCloud!");',
+            loading: true,
+        };
+    }
+
+    async componentDidMount() {
+        const { data, error } = await globalThis.supabaseClient.storage.from("worker-storage").download(globalThis.supabaseClient.auth.user()?.id + "/" + this.props.project + ".js");
+        if (error) {
+            this.setState({ code: 'console.log("Welcome to DenoCloud!");', loading: false });
+        } else {
+            this.setState({ code: await data?.text(), loading: false });
+        }
+    }
+
+    render() {
+        return (
+            <Loading active={this.state.loading}>
+                <CodeMirror
+                    value={this.state.code}
+                    className="pt-2"
+                    height="90vh"
+                    theme={oneDark}
+                    extensions={[basicSetup, javascript({ jsx: true, typescript: true }), javascriptLanguage, javascriptLanguage.data.of({ autocompletion: completeFromGlobalScope }), autocompletion()]}
+                    onChange={(value, viewUpdate) => {
+                        console.log("value:", value);
+                    }}
+                />
+            </Loading>
+        );
+    }
+}
+
 export default function Editor() {
     return (
-        <CodeMirror
-            value='console.log("Welcome to DenoCloud!");'
-            height="500px"
-            theme={oneDark}
-            extensions={[javascript({ jsx: true }), javascriptLanguage.data.of({ autocompletion: completeFromGlobalScope }), autocompletion({ activateOnTyping: true })]}
-            onChange={(value, viewUpdate) => {
-                console.log("value:", value);
-            }}
-        />
+        <div>
+            <EditorComponent project={useParams().project || ""} />
+        </div>
     );
 }
--- frontend/src/routes/dashboard/project/index.tsx
+++ frontend/src/routes/dashboard/project/index.tsx
@@ -1,12 +1,148 @@
-import { Typography } from "@supabase/ui";
+import { Typography, Card, Button } from "@supabase/ui";
 import { ProjectState, BadgeColor, StateColors } from "../index";
+import { buildChartTheme, Axis, Grid, LineSeries, Tooltip, XYChart, TooltipProvider } from "@visx/xychart";
+import { ParentSize } from "@visx/responsive";
+import { appleStock } from '@visx/mock-data';
+import { Text as TextVisx } from "@visx/text";
+import { Link } from "react-router-dom";
 
 const { Text } = Typography;
+
+const data = appleStock.slice(1250);
+
+const accessors = {
+    xAccessor: (d: any) => new Date(d.date),
+    yAccessor: (d: any) => d.close,
+}
+
+const theme = buildChartTheme({
+    backgroundColor: "transparent",
+    colors: ["#65d9a5"],
+    tickLength: 4,
+    gridColor: "#2a2a2a",
+    gridColorDark: "#2a2a2a",
+    xAxisLineStyles: {
+        stroke: "#2a2a2a",
+    },
+    yAxisLineStyles: {
+        stroke: "#2a2a2a",
+    },
+    xTickLineStyles: {
+        stroke: "#2a2a2a",
+    },
+    yTickLineStyles: {
+        stroke: "#2a2a2a",
+    }
+});
 
 export default function ProjectIndex() {
     return (
         <div>
-            <Text>Project</Text>
+            <Button size="medium" className="mt-3 mr-10 float-right"><Link to="editor">Edit</Link></Button>
+            <div className="columns-3 w-full py-5 px-10 h-auto">
+                <Card className={`card bg-[#1f1f1f] mb-2 border-[#2a2a2a] flex-col-reverse`} cover={
+                    <div className="p-5 pt-0 h-[40vh]">
+                        <ParentSize>
+                            {(parent) => {
+                                return (<TooltipProvider hideTooltipDebounceMs={0}>
+                                    <XYChart height={parent.height} width={parent.width} xScale={{ type: 'time' }} yScale={{ type: 'linear' }} theme={theme}>
+                                        <Axis orientation="left" numTicks={5} />
+                                        <Axis orientation="bottom" />
+                                        <Grid />
+                                        <LineSeries dataKey="data" data={data} {...accessors} />
+                                        <Tooltip
+                                            snapTooltipToDatumX
+                                            snapTooltipToDatumY
+                                            showVerticalCrosshair
+                                            showDatumGlyph
+                                            unstyled
+                                            applyPositionStyle
+                                            renderTooltip={({ tooltipData }) => (
+                                                <div className="text-white text-sm p-2 bg-[#1f1f1f] border border-[#2a2a2a] rounded">
+                                                    <span className="font-bold">{accessors.xAccessor(tooltipData?.nearestDatum?.datum).toLocaleString("en-US", { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' })}</span>
+                                                    <br />
+                                                    {accessors.yAccessor(tooltipData?.nearestDatum?.datum) + " Requests"}
+                                                </div>
+                                            )}
+                                        />
+                                    </XYChart>
+                                </TooltipProvider>)
+                            }}
+                        </ParentSize>
+                    </div>
+                }>
+                    <span className="font-normal text-gray-1100"><Text>Requests</Text><br /></span>
+                    <span className="font-bold text-lg"><Text>12</Text></span>
+                </Card>
+                <Card className={`card bg-[#1f1f1f] mb-2 border-[#2a2a2a] flex-col-reverse`} cover={
+                    <div className="p-5 pt-0 h-[40vh]">
+                        <ParentSize>
+                            {(parent) => {
+                                return (<TooltipProvider hideTooltipDebounceMs={0}>
+                                    <XYChart height={parent.height} width={parent.width} xScale={{ type: 'time' }} yScale={{ type: 'linear' }} theme={theme}>
+                                        <Axis orientation="left" numTicks={5} />
+                                        <Axis orientation="bottom" />
+                                        <Grid />
+                                        <LineSeries dataKey="data" data={data} {...accessors} />
+                                        <Tooltip
+                                            snapTooltipToDatumX
+                                            snapTooltipToDatumY
+                                            showVerticalCrosshair
+                                            showDatumGlyph
+                                            unstyled
+                                            applyPositionStyle
+                                            renderTooltip={({ tooltipData }) => (
+                                                <div className="text-white text-sm p-2 bg-[#1f1f1f] border border-[#2a2a2a] rounded">
+                                                    <span className="font-bold">{accessors.xAccessor(tooltipData?.nearestDatum?.datum).toLocaleString("en-US", { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' })}</span>
+                                                    <br />
+                                                    {accessors.yAccessor(tooltipData?.nearestDatum?.datum) + " Errors"}
+                                                </div>
+                                            )}
+                                        />
+                                    </XYChart>
+                                </TooltipProvider>)
+                            }}
+                        </ParentSize>
+                    </div>
+                }>
+                    <span className="font-normal text-gray-1100"><Text>Errors</Text><br /></span>
+                    <span className="font-bold text-lg"><Text>12</Text></span>
+                </Card>
+                <Card className={`card bg-[#1f1f1f] mb-2 border-[#2a2a2a] flex-col-reverse`} cover={
+                    <div className="p-5 pt-0 h-[40vh]">
+                        <ParentSize>
+                            {(parent) => {
+                                return (<TooltipProvider hideTooltipDebounceMs={0}>
+                                    <XYChart height={parent.height} width={parent.width} xScale={{ type: 'time' }} yScale={{ type: 'linear' }} theme={theme}>
+                                        <Axis orientation="left" numTicks={5} />
+                                        <Axis orientation="bottom" />
+                                        <Grid />
+                                        <LineSeries dataKey="data" data={data} {...accessors} />
+                                        <Tooltip
+                                            snapTooltipToDatumX
+                                            snapTooltipToDatumY
+                                            showVerticalCrosshair
+                                            showDatumGlyph
+                                            unstyled
+                                            applyPositionStyle
+                                            renderTooltip={({ tooltipData }) => (
+                                                <div className="text-white text-sm p-2 bg-[#1f1f1f] border border-[#2a2a2a] rounded">
+                                                    <span className="font-bold">{accessors.xAccessor(tooltipData?.nearestDatum?.datum).toLocaleString("en-US", { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' })}</span>
+                                                    <br />
+                                                    {accessors.yAccessor(tooltipData?.nearestDatum?.datum) + " ms"}
+                                                </div>
+                                            )}
+                                        />
+                                    </XYChart>
+                                </TooltipProvider>)
+                            }}
+                        </ParentSize>
+                    </div>
+                }>
+                    <span className="font-normal text-gray-1100"><Text>Average Response Time (ms)</Text><br /></span>
+                    <span className="font-bold text-lg"><Text>12 ms</Text></span>
+                </Card>
+            </div>
         </div>
     );
 }
